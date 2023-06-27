cb33ca1da175f76a4e517d0d303480b496a920c8
kristina <tinamas@yandex.ru>
2023-06-27 19:32:44 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "regenerator-runtime/runtime";
+import App from "./components/App";
+import Comics from "./components/Comics";
+
+(async () => {
+  await App.render();
+
+  Comics.eventListener();
+})();
